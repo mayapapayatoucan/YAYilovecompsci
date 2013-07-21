5f2fcf173d896386442fe978ad8051ca4604d316
mayapapayatoucan <mkk@berkeley.edu>
2013-07-20 19:32:33 -0700

    Update ExpressionTest.java

--- ExpressionTest.java
+++ ExpressionTest.java
@@ -1,47 +1,46 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
 public class ExpressionTest {
-	
-	public static void main (String[] args) {
 
-		Expression expressingmyself;
-		Expression stillexpressing;
 
+	public boolean testhelper(String S) {
+		boolean flag = false;
+		
 		try {
-
-	expressingmyself = new Expression("a");
-		stillexpressing = new Expression("((p&q)=>q)");
-		Expression youdontknowme = new Expression("~((p|q)&p)");
-	//Expression expressingmyself2 = new Expression("(&(p|q))");
-	//	Expression mybloodyexpression = new Expression("(p=q)");
-		Expression hashtagamandabynes = new Expression("A");
-		Expression hashtagjokesaboutmyd = new Expression("(pq)");
-		Expression dpicks = new Expression("(&=>|)");
-		Expression theresalwaysmoneyinthebananastand = new Expression("");
-		Expression wut = new Expression("(p=>))");
-		Expression leanback = new Expression("((p=>q)");
-		Expression winstoned = new Expression("(p&)=>q()");
-		Expression quillin = new Expression("(p#q)");
-		Expression spacedout = new Expression("(p => q)");
-		Expression luvsawoman = new Expression("");
-/*
-		System.out.println("EM2 Root: " + expressingmyself2.getRoot());
-		System.out.println(expressingmyself.getRoot());
-		System.out.println(stillexpressing.getRoot());
-		System.out.println("Should be &: " + stillexpressing.getLeft().getRoot());
-		System.out.println("Should be p: " + stillexpressing.getLeft().getLeft().getRoot());
-		System.out.println("Should be ~: " + youdontknowme.getRoot());
-		System.out.println("Should be &: " + youdontknowme.getRight().getRoot());
-		System.out.println("Should be |: " + youdontknowme.getRight().getLeft().getRoot());
-		System.out.println("Should be p: " + youdontknowme.getRight().getRight().getRoot());
-*/
-	}	catch (IllegalLineException e) {
-			System.out.println(e.getMessage());
+		Expression testExp = new Expression(S);		
+		}
+		catch (IllegalLineException e) {
+		flag = true;
+		}
+		
+		if (flag) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	
 	}
-
-
-
-
-
-
+	
+	@Test
+	public void testExceptions () {
+		//commented ones are the ones which tests fail
+		assertTrue(testhelper("A"));
+		assertTrue(testhelper("(&=>|)"));
+	//	assertTrue(testhelper("(pq)"));
+		assertTrue(testhelper("(p=>))"));
+//		assertTrue(testhelper("((p=>)"));
+		assertTrue(testhelper("(p&)=>q()"));
+	//	assertTrue(testhelper("(p#q)"));
+		assertTrue(testhelper("(p => q)"));
+		assertTrue(testhelper(""));
+		
+		
+		
+		
 	}
 
 }
