92f13299eabba68429e59ba52a9078623795aedb
atomicunicorn <zigottesman@gmail.com>
2013-07-23 22:22:49 -0700

    Update ProofTest.java

--- ProofTest.java
+++ ProofTest.java
@@ -10,7 +10,6 @@ public class ProofTest {
 		try {
 			assertTrue(Proof.isMP(new Expression("(p=>q)"), new Expression("p"), new Expression("q")));
 			assertFalse(Proof.isMP(new Expression("(p=>q)"), new Expression("q"), new Expression("q")));
-			assertTrue(Proof.isMT(new Expression("(p=>q)"), new Expression("~q"), new Expression("~p")));
 			assertFalse(Proof.isMP(new Expression("(p=>q)"), new Expression("~q"), new Expression("~q")));
 			assertTrue(Proof.isIC(new Expression("q"), new Expression("(p=>q)")));
 			assertTrue(Proof.isC(new Expression("p"), new Expression("~p")));
@@ -19,4 +18,16 @@ public class ProofTest {
 		} catch (IllegalLineException e){
 		}
 	}
+@Test
+	public void testIsModusTollens() {
+		try {
+			assertTrue(Proof.isMT(new Expression("(p=>q)"), new Expression("~q"), new Expression("~p")));
+			assertTrue(Proof.isMT(new Expression("(r&s)=>(t|u)"), new Expression("~(t|u)"), new Expression("~(r&s)")));
+			assertTrue(Proof.isMT(new Expression("~(z&x)=>~(h|g)"), new Expression("~~(z&x)"), new Expression("~~(h|g)")));
+			assertTrue(Proof.isMT(new Expression("~(z&x)=>~(h|g)"), new Expression("(z&x)"), new Expression("(h|g)")));
+			
+		} catch (IllegalLineException e){
+		}
+	}
+
 }
